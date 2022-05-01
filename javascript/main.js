f177434a156064ce3ffc8843527151f76e5eb343
Cristian De La Quintana <cri.dlq@gmail.com>
2022-04-30 22:59:26 -0300

    primera version

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+let i;
+
+for (let i=1; i<=12 ; i++){
+
+    switch (i){
+        case 1 : console.log("el 1 es Enero");
+        break;
+
+        case 2 : console.log("el 2 es Febrero");
+        break;
+
+        case 3 : console.log("el 3 es Marzo");
+        break;
+        case 4 : console.log("el 4 es Abril");
+        break;
+        case 5 : console.log("el 5 es Mayo");
+        break;
+        case 6 : console.log("el 6 es Junio");
+        break;
+        case 7 : console.log("el 7 es Julio");
+        break;
+        case 8 : console.log("el 8 es Agosto");
+        break;
+        case 9 : console.log("el 9 es Septiembre");
+        break;
+        case 10 : console.log("el 10 es Octubre");
+        break;
+        case 11 : console.log("el 11 es Noviembre");
+        break;
+        case 12 : console.log("el 12 es Diciembre");
+        break;
+        
+
+
+
+    }
+}
+
